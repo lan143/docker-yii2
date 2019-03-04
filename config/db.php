451814799d66c4767a2893e11ce35b7dca09831e
Artyom Kravchenko <lan143@lan143.ru>
2019-03-04 22:06:05 +0300

    Init code

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=dev',
+    'username' => 'root',
+    'password' => 'dev',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
